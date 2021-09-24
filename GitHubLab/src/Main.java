ecebbfd28f9d4510a5019aed6f888137fa97b2d4
Thanh <truongvanthanh1@dtu.edu.vn>
2021-09-24 14:57:10 +0700

    ok

--- GitHubLab/src/Main.java
+++ GitHubLab/src/Main.java
@@ -1,4 +1,5 @@
 class Main {
+<<<<<<< HEAD
 	public static void main(String arg[]) {
 		SoPhuc a = new SoPhuc(2.f, 3.f);
 		SoPhuc b = new SoPhuc(4.f, 5.f);
@@ -6,4 +7,7 @@ class Main {
 		System.out.println(b.toString());
 		System.out.println(a.cong(b).toString());
 	}
+=======
+	
+>>>>>>> branch 'main' of https://github.com/truongvanthanh95/CS414G_Lab2
 }
